c5761677fb307d2380f9c9a27df48fc728013a73
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2023-10-09 23:25:59 +0200

    yeah buddy

---
+++ article.js
@@ -0,0 +1,14 @@
+fetch("http://localhost:3000/blog/articles/" + location.search.replace("?", "")).then(async (response) => {
+    const article = await response.json();
+    let html = "";
+    
+    html += `
+        <h2>${article.title}</h2>
+        <h5>${article.publishData}</h5>
+        <div>
+            <p>${article.description}</p>
+        </div>
+    `;
+    
+    document.getElementById("container").innerHTML = html;
+})
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-fetch("http://localhost:3000/blog/getArticles").then(async (response) => {
+fetch("http://localhost:3000/blog/articles").then(async (response) => {
     const articles = await response.json();
     let html = "";
     for (const article of articles) {
         html += `
-            <a href="/article.html">
+            <a href="/article.html?${article.id}">
                 <article>
                     <p>${article.title}</p>
                     <span>${article.description}</span>
